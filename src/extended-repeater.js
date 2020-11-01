92765b8908cf24f590d093c85c2b91d2e50e16a9
Tamara <yaroslavcevatamara@gmail.com>
2020-11-01 19:40:43 +0300

    feat: implement extended repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,44 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+
+  let newStr = str;
+  let newStrAdd = '';
+  let repeatTimes = options.repeatTimes; // repeatTimes sets the number of repetitions of the str;
+  let separator = options.separator; // separator is a string separating repetitions of the str;
+  let addition = options.addition; // addition is an additional string that will be added to each repetition of the str;
+  let additionRepeatTimes = options.additionRepeatTimes; // additionRepeatTimes sets the number of repetitions of the addition;
+  let additionSeparator = options.additionSeparator; // additionSeparator is a string separating repetitions of the addition
   
+  const createAddition = () => {
+    let addStr = '';
+    if (!additionRepeatTimes) {
+      !addition ? addStr : addStr = addition; 
+    } else {
+      addStr = addition;
+      for (i = 0; i < additionRepeatTimes - 1; i++) {
+        !additionSeparator ? additionSeparator = '|' :  additionSeparator;
+        addStr = `${addStr}${additionSeparator}${addition}`;
+      }
+    }
+  //console.log(addStr);
+  return addStr;
+  }
+  
+  if (!options) return newStr;
+  if (!repeatTimes) {
+    newStrAdd = createAddition();
+    newStr += newStrAdd;
+  } else {
+    newStrAdd = createAddition();
+    newStr += newStrAdd
+    for (i = 0; i < repeatTimes - 1; i++) {
+      !separator ? separator = '+' :  separator;
+      newStr = `${newStr}${separator}${str}${newStrAdd}`;
+    }
+
+  }
+
+  //console.log(newStr);
+  return newStr;  
+};
